8c52fe7044ee088e42b4d3e3367482487553d552
Tanisha-987 <coding.official441@gmail.com>
2025-04-30 13:38:00 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+require('dotenv').config();
+
+const connectDB = async () =>{
+    try{
+        await mongoose.connect(process.env.MONGO_URL,{
+            useNewUrlParser:true,
+            useUnifiedTopology :true
+        })
+        console.log("connected successfully");
+    }
+    catch(error){
+        console.log(error);
+        console.log("error occured");
+    }
+}
+
+module.exports=connectDB
---
+++ models/course.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose")
+
+const courseSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    src: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true }
+  });
+  
+  const Course = mongoose.model('CourseCollection', courseSchema);
+  
+  module.exports = Course;
